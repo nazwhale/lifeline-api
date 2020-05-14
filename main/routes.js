da2804a35343a5377ec125ae0c257fd510666971
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-05-14 15:51:12 +0100

    validate jwt with google auth, error handling, api endpoint renaming

--- main/routes.js
+++ main/routes.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var pool = require("./db");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 /*
   Users Table
@@ -11,7 +13,7 @@ router.get("/", function(req, res, next) {
 });
 
 // Create new user
-router.post("/post/user", (req, res, next) => {
+router.post("/user", (req, res, next) => {
   console.log("🙏", req.body);
   const { profile } = req.body;
 
@@ -29,21 +31,31 @@ router.post("/post/user", (req, res, next) => {
 });
 
 // Read existing user
-router.get("/get/user", (req, res, next) => {
+router.get("/user", verify, (req, res, next) => {
   const email = req.query.email;
+
   console.log("🤝", email);
+
   pool.query(
     `SELECT * FROM users
               WHERE email=$1`,
     [email],
     (q_err, q_res) => {
-      res.json(q_res.rows);
+      if (q_res.rows.length === 0) {
+        console.log("about to 404...");
+        res.status(404).json({
+          code: "user_not_found",
+          name: "User not found"
+        });
+      } else {
+        res.json(q_res.rows);
+      }
     }
   );
 });
 
 // Update existing user's last login
-router.put("/put/user", (req, res, next) => {
+router.put("/user", (req, res, next) => {
   console.log("🙏", req.body);
   const { lastLogin, email } = req.body;
   const values = [lastLogin, email];
@@ -58,5 +70,25 @@ router.put("/put/user", (req, res, next) => {
     }
   );
 });
+
+async function verify(req, res, next) {
+  const token = req.headers["authorization"];
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  console.log("🎫", ticket);
+  const payload = ticket.getPayload();
+  console.log("💵", payload);
+  const userid = payload["sub"];
+  console.log("🙋‍♀️", userid);
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+
+  next();
+}
 
 module.exports = router;
--- main/schema.sql
+++ main/schema.sql
@@ -1,9 +1,18 @@
+# other fields?
+# soft deleted_at field?
+# external_login_id column
+# login type column
+# timestamp for everything? - enforce we're storing everything in UTC on the backend
+# replace NOW() with timezone('utc', NOW())
+
+
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE,
   date_created DATE,
   last_login DATE
 );
+
 
 CREATE TABLE experiences (
   pid SERIAL PRIMARY KEY,
