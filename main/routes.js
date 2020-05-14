7099ae8e5956478f377c7708cfd8af4dd186244c
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-05-14 12:50:48 +0100

    post and update reqs

--- main/routes.js
+++ main/routes.js
@@ -10,11 +10,16 @@ router.get("/", function(req, res, next) {
   res.send("API is working woop!");
 });
 
+// Create new user
 router.post("/post/user", (req, res, next) => {
-  const values = [req.body.profile.email];
+  console.log("🙏", req.body);
+  const { profile } = req.body;
+
+  const values = [profile.email, profile.lastLogin];
+  console.log("⚡️", values);
   pool.query(
-    `INSERT INTO users(email, date_created)
-              VALUES($1, NOW())
+    `INSERT INTO users(email, date_created, last_login)
+              VALUES($1, NOW(), $2)
               ON CONFLICT DO NOTHING`,
     values,
     (q_err, q_res) => {
@@ -23,13 +28,31 @@ router.post("/post/user", (req, res, next) => {
   );
 });
 
+// Read existing user
 router.get("/get/user", (req, res, next) => {
   const email = req.query.email;
-  console.log(email);
+  console.log("🤝", email);
   pool.query(
     `SELECT * FROM users
               WHERE email=$1`,
     [email],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+// Update existing user's last login
+router.put("/put/user", (req, res, next) => {
+  console.log("🙏", req.body);
+  const { lastLogin, email } = req.body;
+  const values = [lastLogin, email];
+
+  pool.query(
+    `UPDATE users
+              SET last_login = $1
+              WHERE email=$2`,
+    values,
     (q_err, q_res) => {
       res.json(q_res.rows);
     }
