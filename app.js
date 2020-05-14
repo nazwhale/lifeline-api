055f43c6b8102b17cb5a2e9cfdc0525dd8e72925
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-05-14 16:40:11 +0100

    delete unused routes

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
-var apiRoutes = require("./main/routes.js");
 
 var app = express();
 
@@ -23,10 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
-app.use("/api", apiRoutes);
+// todo: delete old routes
+app.use("/api", indexRouter);
+app.use("/api/user", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+/*
+other fields?
+  rename user_id to id
+  soft deleted_at field?
+  external_login_id column
+  login type column
+  timestamp for everything? - enforce we're storing everything in UTC on the backend
+  replace NOW() with timezone('utc', NOW())
+ */
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE,
+  date_created DATE,
+  last_login DATE
+);
+
+/*
+  rename pid to id
+  rename user_id to id
+*/
+CREATE TABLE experiences (
+  pid SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  start_date TIMESTAMP,
+  end_date TIMESTAMP,
+  user_id INT REFERENCES users(user_id)
+);
